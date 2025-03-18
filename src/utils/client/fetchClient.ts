b45a3cb87a23d410876c12be045ae19077aa3898
Supakrit <supakrit.apo@cp.co.th>
2025-03-18 21:37:32 +0700

    specify route url

--- src/utils/client/fetchClient.ts
+++ src/utils/client/fetchClient.ts
@@ -1,6 +1,6 @@
 // Choose the base URL based on the stage.
 const baseURL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080"
+  process.env.NEXT_PUBLIC_API_URL ?? "https://netviser-app-440043701198.asia-east1.run.app"
 
 /**
  * A custom fetch client that:
