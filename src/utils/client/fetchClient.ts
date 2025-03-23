dfb880080972dbf577c14bc87b4bea7057d348d1
Supakrit <supakrit.apo@cp.co.th>
2025-03-23 12:47:03 +0700

    update backend url

--- src/utils/client/fetchClient.ts
+++ src/utils/client/fetchClient.ts
@@ -2,7 +2,7 @@ const env = process.env.NEXT_PUBLIC_ENV ?? "local"
 
 let baseURL = "http://localhost:8080"
 if (env === "production") {
-  baseURL = "https://netviser-app-dev-440043701198.asia-east1.run.app"
+  baseURL = "https://netviser-backend-440043701198.asia-southeast1.run.app"
 }
 
 /**
